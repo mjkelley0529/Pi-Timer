36d41c291679d3eb7fbcb8060e79792005267780
mkelley <xkelley0529@gmail.com>
2019-11-19 11:31:54 -0600

    Initial Commit

---
+++ src/Main/PiTimer.java
@@ -0,0 +1,229 @@
+package Main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PiTimer extends JPanel implements ActionListener {
+    //Variable Declaration
+    private static final String TITLE ="Timer", VERID="0.2";
+    //Logic Variables
+    private int[] times={0,0,0}, timeMax={200,59,59}, savedTimes={0,0,0}, lastTimes={0,0,0};
+    private long startTime=-1;
+    private long elapsedTime=0;
+    private Timer timer;
+    //Interface Variables
+    private JLabel[] timeDisplays =new JLabel[times.length];
+    private JButton[] timeAdjusters =new JButton[timeDisplays.length*2],
+            interfaceButtons =new JButton[3];
+    private String[] iButtonStrings ={"Clear","Reset","Start","Stop"};
+    private JPanel[] containerPanes =new JPanel[4];
+    //Aesthetic Variables
+    private static Font font=new Font("Arial", Font.BOLD, 72);
+    private static Color backgroundColor=Color.decode("#000000"),
+            foregroundColor=Color.decode("#FFFFFF"),
+            transparent=new Color(0,0,0,0);
+    private GridLayout frameLay=new GridLayout(2,1,5,5),
+            containerLay[]=new GridLayout[containerPanes.length];
+    //Constructor
+    private PiTimer() {
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
+        //Initialize Variables
+        for(int i=0;i<timeDisplays.length;i++) {
+            timeDisplays[i]=new JLabel(String.valueOf(times[i]));
+            timeDisplays[i].setFocusable(false);
+            timeDisplays[i].setFont(font);
+            timeDisplays[i].setHorizontalAlignment(JTextField.CENTER);
+            timeDisplays[i].setBackground(backgroundColor);
+            timeDisplays[i].setForeground(foregroundColor);
+        }
+        for(int i=0;i<timeAdjusters.length;i++) {
+            timeAdjusters[i]=new JButton();
+            if(i<timeAdjusters.length/2) {
+                timeAdjusters[i].setText("/\\");
+            } else {
+                timeAdjusters[i].setText("\\/");
+            }
+            timeAdjusters[i].addActionListener(this);
+            timeAdjusters[i].setFocusable(false);
+            timeAdjusters[i].setFont(font);
+            timeAdjusters[i].setBackground(backgroundColor);
+            timeAdjusters[i].setForeground(foregroundColor);
+        }
+        for(int i=0;i<interfaceButtons.length;i++) {
+            interfaceButtons[i]=new JButton(iButtonStrings[i]);
+            interfaceButtons[i].addActionListener(this);
+            interfaceButtons[i].setFocusable(false);
+            interfaceButtons[i].setFont(font);
+            interfaceButtons[i].setBackground(backgroundColor);
+            interfaceButtons[i].setForeground(foregroundColor);
+        }
+        containerLay[0]=new GridLayout(3, timeDisplays.length+2, 5,5);
+        containerLay[1]=new GridLayout(2, 1,5,5);
+        containerLay[2]=new GridLayout(1,2,5,5);
+        containerLay[3]=new GridLayout(1,1,5,5);
+        for(int i=0;i<containerPanes.length;i++) {
+            containerPanes[i]=new JPanel();
+            containerPanes[i].setLayout(containerLay[i]);
+            containerPanes[i].setFocusable(false);
+            containerPanes[i].setBackground(transparent);
+        }
+        //Frame Setup
+        setBackground(backgroundColor);
+        setLayout(frameLay);
+
+        for(int i=0;i<timeDisplays.length;i++) {
+            containerPanes[0].add(timeAdjusters[i]);
+        }
+        for(int i=0;i<timeDisplays.length;i++) {
+            containerPanes[0].add(timeDisplays[i]);
+        }
+        for(int i=timeDisplays.length;i<timeDisplays.length*2;i++) {
+            containerPanes[0].add(timeAdjusters[i]);
+        }
+        containerPanes[2].add(interfaceButtons[0]);
+        containerPanes[2].add(interfaceButtons[1]);
+        containerPanes[3].add(interfaceButtons[2]);
+        for(int i=2;i<containerPanes.length;i++) {
+            containerPanes[1].add(containerPanes[i]);
+        }
+        for(int i=0;i<2;i++) {
+            add(containerPanes[i]);
+        }
+    }
+    //Utility Methods
+    private void updateTimes() {
+        for(int i=0;i<times.length;i++) {
+            if (times[i]<0) {
+                times[i]=timeMax[i];
+            }
+            if(times[i]>timeMax[i]) {
+                times[i]=0;
+            }
+            System.out.print(times[i]+" ");
+        }
+        System.out.println();
+    }
+    private void updateDisplays() {
+        updateTimes();
+        for(int i=0;i<timeDisplays.length;i++) {
+            timeDisplays[i].setText(String.valueOf(times[i]));
+            timeDisplays[i].repaint();
+        }
+        repaint();
+    }
+    //Inherited Methods
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object s=e.getSource();
+        for(int i=0;i<timeAdjusters.length;i++) {
+            if (s.equals(timeAdjusters[i])) {
+                System.err.println("Time Adjuster "+i);
+                if(i>=timeDisplays.length) {
+                    times[i-timeDisplays.length]--;
+                } else {
+                    times[i]++;
+                }
+                updateDisplays();
+            }
+        }
+        if(s.equals(interfaceButtons[0])) {
+            if(interfaceButtons[2].getText().equals(iButtonStrings[3])) {
+                interfaceButtons[2].doClick();
+            }
+            for(int i=0;i<times.length;i++) {
+                times[i]=0;
+            }
+            updateDisplays();
+        }
+        if(s.equals(interfaceButtons[1])) {
+            System.arraycopy(savedTimes, 0, times, 0, savedTimes.length);
+            updateDisplays();
+            if(interfaceButtons[2].getText().equals(iButtonStrings[3])) {
+                interfaceButtons[2].doClick();
+            }
+        }
+        if(s.equals(interfaceButtons[2])) {
+            System.err.println(interfaceButtons[2].getText());
+            boolean runnable=false;
+            for(int i:times) {
+                if (i != 0) {
+                    runnable = true;
+                    break;
+                }
+            }
+            if (interfaceButtons[2].getText().equals(iButtonStrings[2])&&runnable) {
+                interfaceButtons[2].setText(iButtonStrings[3]);
+                System.arraycopy(times, 0, savedTimes, 0, times.length);
+                for(JButton i:timeAdjusters) {
+                    i.setEnabled(false);
+                }
+                timer=new Timer(0, e1 -> {
+                    if (startTime < 0) {
+                        startTime = System.currentTimeMillis();
+                    }
+                    elapsedTime=System.currentTimeMillis()-startTime;
+                    if(elapsedTime%1000==0) {
+                        times[times.length-1]--;
+                        updateTimes();
+                        for(int i=times.length-1;i>0;i--) {
+                            System.err.println(i+" "+times[i]+" "+timeMax[i]+" "+lastTimes[i]);
+                            if ((times[i] == timeMax[i]) && (lastTimes[i] == 0)) {
+                                times[i - 1]--;
+                                updateTimes();
+                            }
+                        }
+                        updateDisplays();
+                    }
+                    boolean done=true;
+                    for(int i:times) {
+                        if (i != 0) {
+                            done = false;
+                            break;
+                        }
+                    }
+                    if (done) {
+                        interfaceButtons[2].doClick();
+                    }
+                    System.arraycopy(times, 0, lastTimes, 0, times.length);
+                });
+                timer.start();
+            } else if (interfaceButtons[2].getText().equals(iButtonStrings[3])) {
+                interfaceButtons[2].setText(iButtonStrings[2]);
+                timer.stop();
+                for(JButton i:timeAdjusters) {
+                    i.setEnabled(true);
+                }
+            }
+        }
+    }
+    //Main Method
+    public static void main(String[] args) {
+        PiTimer mainTimer=new PiTimer();
+        PiTimer secondTimer=new PiTimer();
+        JPanel spacer=new JPanel();
+        spacer.setFocusable(false);
+        spacer.setBackground(backgroundColor);
+
+        JFrame mainFrame=new JFrame();
+        mainFrame.setTitle(TITLE+" "+VERID);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        mainFrame.setUndecorated(false);
+        mainFrame.setLayout(new GridLayout(1,3));
+        mainFrame.add(mainTimer);
+        mainFrame.add(spacer);
+        mainFrame.add(secondTimer);
+        mainFrame.setVisible(true);
+    }
+}
