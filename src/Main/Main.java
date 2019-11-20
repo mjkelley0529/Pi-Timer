f26a77b2a806f120ae3f6ec1a273a9f6dcbb409e
mkelley <xkelley0529@gmail.com>
2019-11-20 12:44:10 -0600

    Togglable Start Buttons

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,11 +5,104 @@ import com.pi4j.util.CommandArgumentParser;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+public class Main extends JFrame implements ActionListener {
     //Variable Declaration
     private static final String TITLE ="Timer", VERID="0.3";
-    private static final Color bgColor=Color.decode("#000000"), fgColor=Color.decode("#FFFFFF");
+    //UI Variables
+    PiTimer[] timers=new PiTimer[2];
+    JPanel spacer=new JPanel(), containerPane=new JPanel(), framePane=new JPanel();
+    JToggleButton start=new JToggleButton("Start");
+    //Aesthetic Variables
+    private Color bgColor=Color.decode("#000000"),
+            fgColor=Color.decode("#FFFFFF"),
+            startColor=Color.decode("#009900"),
+            stopColor=Color.decode("#990000");
+    private Font font=new Font("Arial", Font.BOLD, 32);
+    Main() {
+        for(int i=0;i<timers.length;i++) {
+            timers[i]=new PiTimer(bgColor, fgColor,startColor,stopColor,font);
+        }
+        //Pi Interfacing
+        /*GpioController gpio;
+        Pin[] pin=new Pin[timers.length];
+        GpioPinDigitalOutput[] output=new GpioPinDigitalOutput[timers.length];
+        try {
+            gpio = GpioFactory.getInstance(); //Aquired code from: https://dzone.com/articles/simple-steps-to-develop-smart-light-java-amp-iot
+            pin[0] = CommandArgumentParser.getPin(RaspiPin.class, RaspiPin.GPIO_04);
+            pin[1] = CommandArgumentParser.getPin(RaspiPin.class, RaspiPin.GPIO_05);
+            for(int i=0;i<output.length;i++) {
+                output[i] = gpio.provisionDigitalOutputPin(pin[i], "My Output", PinState.HIGH);
+                timers[i].setOutput(output[i]);
+            }
+        } catch(Exception e) {
+            gpio=null;
+            pin=null;
+            output=null;
+        }*/
+        spacer.setFocusable(false);
+        spacer.setBackground(bgColor);
+
+        start.setFocusable(false);
+        start.addActionListener(this);
+        start.setBackground(startColor);
+        start.setFont(font);
+
+        containerPane.setLayout(new GridLayout(3,1,5,5));
+        containerPane.setFocusable(false);
+        containerPane.setBackground(bgColor);
+        containerPane.add(new JLabel(new ImageIcon("/src/Resources/logo.png")));
+        containerPane.add(spacer);
+        containerPane.add(start);
+
+        framePane.setLayout(new GridLayout(1,3,10,10));
+        framePane.setBackground(bgColor);
+        framePane.setFocusable(false);
+        framePane.add(timers[0]);
+        framePane.add(containerPane);
+        framePane.add(timers[1]);
+
+        setTitle(TITLE+" "+VERID);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);
+        setLayout(new GridLayout(1,1));
+        setBackground(bgColor);
+        setForeground(fgColor);
+        add(framePane);
+        setVisible(true);
+    }
+    private void setTimers(boolean started) {
+        if(started) {
+            for(int i=0;i<timers.length;i++) {
+                if(timers[i].isRunnable()) {
+                    timers[i].start();
+                } else {
+                    start.setSelected(false);
+                    setTimers(false);
+                }
+            }
+        } else {
+            for (int i = 0; i < timers.length; i++) {
+                timers[i].stop();
+            }
+        }
+    }
+    public void actionPerformed(ActionEvent evt) {
+        if(evt.getSource()==start) {
+            if(start.isSelected()) {
+                setTimers(true);
+                start.setText("Stop");
+                start.setBackground(stopColor);
+            } else {
+                setTimers(false);
+                start.setText("Start");
+                start.setBackground(startColor);
+            }
+        }
+    }
     //Main
     public static void main(String[] args) {
         //Set UI
@@ -23,39 +116,6 @@ public class Main {
         } catch (Exception e) {
             // If Nimbus is not available, you can set the GUI to another look and feel.
         }
-        PiTimer[] timers=new PiTimer[2];
-        GpioController gpio;
-        Pin[] pin=new Pin[timers.length];
-        GpioPinDigitalOutput[] output=new GpioPinDigitalOutput[timers.length];
-        try {
-            gpio = GpioFactory.getInstance(); //Aquired code from: https://dzone.com/articles/simple-steps-to-develop-smart-light-java-amp-iot
-            pin[0] = CommandArgumentParser.getPin(RaspiPin.class, RaspiPin.GPIO_04);
-            pin[1] = CommandArgumentParser.getPin(RaspiPin.class, RaspiPin.GPIO_05);
-            for(int i=0;i<output.length;i++) {
-                output[i] = gpio.provisionDigitalOutputPin(pin[i], "My Output", PinState.HIGH);
-            }
-        } catch(Exception e) {
-            gpio=null;
-            pin=null;
-            output=null;
-        }
-        for(int i=0;i<timers.length;i++) {
-            timers[i]=new PiTimer(bgColor, fgColor);
-            timers[i].setOutput(output[i]);
-        }
-        JPanel spacer=new JPanel();
-        spacer.setFocusable(false);
-        spacer.setBackground(Color.decode("#000000"));
-
-        JFrame mainFrame=new JFrame();
-        mainFrame.setTitle(TITLE+" "+VERID);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        mainFrame.setUndecorated(true);
-        mainFrame.setLayout(new GridLayout(1,3));
-        mainFrame.add(timers[0]);
-        mainFrame.add(spacer);
-        mainFrame.add(timers[1]);
-        mainFrame.setVisible(true);
+        new Main();
     }
 }
--- src/Main/PiTimer.java
+++ src/Main/PiTimer.java
@@ -4,13 +4,17 @@ import com.pi4j.io.gpio.*;
 import com.pi4j.util.CommandArgumentParser;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PiTimer extends JPanel implements ActionListener {
     //Logic Variables
-    private int[] times={0,0,0}, timeMax={200,59,59}, savedTimes={0,0,0}, lastTimes={0,0,0};
+    private int[] times={0,0,0},
+            timeMax={200,59,59},
+            savedTimes={0,0,0},
+            lastTimes={0,0,0};
     private long startTime=-1;
     private long elapsedTime=0;
     private Timer timer;
@@ -22,14 +26,29 @@ public class PiTimer extends JPanel implements ActionListener {
     private JPanel[] containerPanes =new JPanel[4];
     //Aesthetic Variables
     private Font labelFont=new Font("Courier new", Font.BOLD, 32),
-            buttonFont=new Font(labelFont.getName(), Font.PLAIN, 20);
-    private Color transparent=new Color(0,0,0,0);
-    private GridLayout frameLay=new GridLayout(2,1,5,5),
-            containerLay[]=new GridLayout[containerPanes.length];
+            buttonFont=new Font(labelFont.getName(), labelFont.getStyle(), 20);
+    private Color backgroundColor=Color.decode("#000000"),
+            foregroundColor=Color.decode("#FFFFFF"),
+            transparent=new Color(0,0,0,0),
+            startColor=Color.decode("#009900"),
+            stopColor=Color.decode("#990000");
+    private int gap=10;
     //Pi Variables
     private GpioPinDigitalOutput output=null;
     //Constructor
-    public PiTimer(Color backgroundColor, Color foregroundColor) {
+    PiTimer() {
+        frameSetup();
+    }
+    PiTimer(Color backgroundColor, Color foregroundColor, Color startColor, Color stopColor, Font font) {
+        this.backgroundColor=backgroundColor;
+        this.foregroundColor=foregroundColor;
+        this.startColor=startColor;
+        this.stopColor=stopColor;
+        labelFont=font;
+        buttonFont=new Font(labelFont.getName(), labelFont.getStyle(), labelFont.getSize()/8*5);
+        frameSetup();
+    }
+    private void frameSetup() {
         //Initialize Variables
         for(int i=0;i<timeDisplays.length;i++) {
             timeDisplays[i]=new JLabel(String.valueOf(times[i]));
@@ -51,19 +70,26 @@ public class PiTimer extends JPanel implements ActionListener {
             timeAdjusters[i].setFont(buttonFont);
             timeAdjusters[i].setBackground(backgroundColor);
             timeAdjusters[i].setForeground(foregroundColor);
+            timeAdjusters[i].setBorder(BorderFactory.createRaisedBevelBorder());
         }
         for(int i=0;i<interfaceButtons.length;i++) {
             interfaceButtons[i]=new JButton(iButtonStrings[i]);
             interfaceButtons[i].addActionListener(this);
             interfaceButtons[i].setFocusable(false);
             interfaceButtons[i].setFont(buttonFont);
-            interfaceButtons[i].setBackground(backgroundColor);
-            interfaceButtons[i].setForeground(foregroundColor);
+            if(i==2) {
+                interfaceButtons[i].setBackground(startColor);
+                interfaceButtons[i].setForeground(Color.getHSBColor(startColor.getRed(), startColor.getGreen(), startColor.getBlue()));
+            } else {
+                interfaceButtons[i].setBackground(backgroundColor);
+                interfaceButtons[i].setForeground(foregroundColor);
+            }
         }
-        containerLay[0]=new GridLayout(3, timeDisplays.length+2, 5,5);
-        containerLay[1]=new GridLayout(2, 1,5,5);
-        containerLay[2]=new GridLayout(1,2,5,5);
-        containerLay[3]=new GridLayout(1,1,5,5);
+        GridLayout[] containerLay = new GridLayout[containerPanes.length];
+        containerLay[0]=new GridLayout(3, timeDisplays.length+2, gap,gap);
+        containerLay[1]=new GridLayout(2, 1,gap,gap);
+        containerLay[2]=new GridLayout(1,2,gap,gap);
+        containerLay[3]=new GridLayout(1,1,gap,gap);
         for(int i=0;i<containerPanes.length;i++) {
             containerPanes[i]=new JPanel();
             containerPanes[i].setLayout(containerLay[i]);
@@ -72,6 +98,7 @@ public class PiTimer extends JPanel implements ActionListener {
         }
         //Frame Setup
         setBackground(backgroundColor);
+        GridLayout frameLay = new GridLayout(2, 1, gap, gap);
         setLayout(frameLay);
 
         for(int i=0;i<timeDisplays.length;i++) {
@@ -149,66 +176,72 @@ public class PiTimer extends JPanel implements ActionListener {
             }
         }
         if(s.equals(interfaceButtons[2])) {
-            boolean runnable=false;
-            for(int i:times) {
-                if (i != 0) {
-                    runnable = true;
-                    break;
-                }
-            }
-            if (clickable&&runnable) {
+            if (clickable) {
                 start();
             } else if (!clickable) {
                 stop();
             }
         }
     }
-    private void start() {
-        interfaceButtons[2].setText(iButtonStrings[3]);
-        System.arraycopy(times, 0, savedTimes, 0, times.length);
-        timer=new Timer(0, e1 -> {
-            if (startTime < 0) {
-                startTime = System.currentTimeMillis();
-            }
-            elapsedTime=System.currentTimeMillis()-startTime;
-            if(elapsedTime%1000==0) {
-                times[times.length-1]--;
-                updateTimes();
-                for(int i=times.length-1;i>0;i--) {
-                    if ((times[i] == timeMax[i]) && (lastTimes[i] == 0)) {
-                        times[i - 1]--;
-                        updateTimes();
+    public void start() {
+        if(isRunnable()) {
+            interfaceButtons[2].setText(iButtonStrings[3]);
+            interfaceButtons[2].setBackground(stopColor);
+            interfaceButtons[2].setForeground(Color.getHSBColor(stopColor.getRed(), stopColor.getGreen(), stopColor.getBlue()));
+            System.arraycopy(times, 0, savedTimes, 0, times.length);
+            timer = new Timer(0, e -> {
+                if (startTime < 0) {
+                    startTime = System.currentTimeMillis();
+                }
+                elapsedTime = System.currentTimeMillis() - startTime;
+                if (elapsedTime % 1000 == 0) {
+                    times[times.length - 1]--;
+                    updateTimes();
+                    for (int i = times.length - 1; i > 0; i--) {
+                        if ((times[i] == timeMax[i]) && (lastTimes[i] == 0)) {
+                            times[i - 1]--;
+                            updateTimes();
+                        }
+                    }
+                    updateDisplays();
+                }
+                boolean done = true;
+                for (int i : times) {
+                    if (i != 0) {
+                        done = false;
+                        break;
                     }
                 }
-                updateDisplays();
-            }
-            boolean done=true;
-            for(int i:times) {
-                if (i != 0) {
-                    done = false;
-                    break;
+                if (done) {
+                    stop();
                 }
+                System.arraycopy(times, 0, lastTimes, 0, times.length);
+            });
+            timer.start();
+            try {
+                output.low();
+            } catch (Exception e) {
             }
-            if (done) {
-                stop();
-            }
-            System.arraycopy(times, 0, lastTimes, 0, times.length);
-        });
-        timer.start();
-        try {
-            output.low();
-        } catch(Exception e) {
-            e.printStackTrace();
         }
     }
-    private void stop() {
+    public void stop() {
         interfaceButtons[2].setText(iButtonStrings[2]);
+        interfaceButtons[2].setBackground(startColor);
+        interfaceButtons[2].setForeground(Color.getHSBColor(startColor.getRed(), startColor.getGreen(), startColor.getBlue()));
         timer.stop();
         try {
             output.high();
-        } catch(Exception e) {
-            e.printStackTrace();
+        } catch(Exception e) {}
+    }
+    public boolean isRunnable() {
+        boolean runnable=false;
+        for(int i:times) {
+            if (i != 0) {
+                runnable = true;
+                break;
+            }
         }
+        return runnable;
     }
     public void setOutput(GpioPinDigitalOutput output) {
         this.output=output;
